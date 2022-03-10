779454eaaf66acc6ed172d8edd0ab1521313fa16
Shane Nicholson <shanescottnicholson@gmail.com>
2022-03-09 16:37:15 -0800

    Adds normal map to objects

--- myportfolio/main.js
+++ myportfolio/main.js
@@ -1,6 +1,10 @@
+// Adds imports
 import "./style.css";
 
 import * as THREE from "three";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 // adds scene and renderer
 const scene = new THREE.Scene();
 
@@ -20,12 +24,25 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 //creates geometry and material for a 20-sided die
-const geometry = new THREE.IcosahedronGeometry(10, 1, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347, wireframe: false });
+const geometry = new THREE.IcosahedronGeometry(10, 1, 120, 1000);
+const diceNormalTexture = new THREE.TextureLoader().load("img/metal_map.png");
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347, wireframe: false, normalMap: diceNormalTexture });
 const dice = new THREE.Mesh(geometry, material);
+dice.position.set(0, 20, 0);
 
-//adds dice to scene
 scene.add(dice);
+
+// creates plane
+const geometry2 = new THREE.PlaneGeometry(100, 100);
+const planeNormalTexture = new THREE.TextureLoader().load("img/floor_map.png");
+const material2 = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  side: THREE.DoubleSide,
+  normalMap: planeNormalTexture,
+});
+const plane = new THREE.Mesh(geometry2, material2);
+plane.rotation.x = Math.PI / 2;
+scene.add(plane);
 
 //creates point light
 const pointLight = new THREE.PointLight(0xffffff);
@@ -37,6 +54,13 @@ scene.add(pointLight);
 // creates and adds wireframe to pinpoint location of pointlight
 const lightHelper = new THREE.PointLightHelper(pointLight);
 scene.add(lightHelper);
+
+// creates grid for orientation purposes
+// const gridhelper = new THREE.GridHelper(200, 50);
+// scene.add(gridhelper);
+
+// camera orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // animation recursive function
 function animate() {
